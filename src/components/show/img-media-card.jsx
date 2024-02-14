aca2b82f6aae5c8cab0f5925acaa5797d9ab5450
luckynotrich <rich.rhaskell@gmail.com>
2024-02-13 16:19:19 -0800

    debug card pros/cons

--- src/components/show/img-media-card.jsx
+++ src/components/show/img-media-card.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React,{useState,useEffect} from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -7,18 +7,36 @@ import Button from '@mui/material/Button';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import { Box, CardHeader } from '@mui/material';
+import  List  from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
 export default function ImgMediaCard({ category, rev, toggleItem }) {
   let caption, Text, kategori;
-  React.useEffect(() => {
-    console.log('card rev.name',rev.name,' rev ',rev);
-    console.log('card category.name',category.name,' category ',category);
-    if(rev.pros && rev.pros.length > 0) {rev.pros.map((pro) => {
-      category.pros.find((catPro) => {catPro.id === pro.id && 
-      console.log('catPro.value =',catPro.value ,catPro.id,' === ', pro.id)})})}
-      if(rev.cons && rev.cons.length > 0) {rev.cons.map((con) => {
-        category.cons.find((catCon) => {catCon.id === con.id && 
-        console.log('catCon.value =',catCon.value ,catCon.id,' === ', con.id)})})}
-}, [rev]);
+  const [pros,setPros] = useState([])
+   const [cons,setCons] = useState([]);
+  useEffect(() => {
+    console.log('card rev.name', rev.name, ' rev ', rev);
+    console.log('card category.name', category.name, ' category ', category);
+    if (rev.pros && rev.pros.length > 0) {
+      rev.pros.map((pro) => {
+        category.pros.find((catPro) => {
+          catPro.id === pro.id &&
+            setPros([...pros,{ value: catPro.value, id: catPro.id }]);
+        });
+      });
+      console.log('pros =', pros);
+    }
+    if (rev.cons && rev.cons.length > 0) {
+      rev.cons.map((con) => {
+        category.cons.find((catCon) => {
+          catCon.id === con.id &&
+            setCons([...cons,{ value: catCon.value, id: catCon.id }]);
+        });
+      });
+      console.log('cons', cons);
+    }
+  }, [rev, category]);
   {
     if (rev) {
       (caption = rev.name), (Text = rev.text), (kategori = category.name);
@@ -34,7 +52,8 @@ export default function ImgMediaCard({ category, rev, toggleItem }) {
       sx={{
         width: '70%',
         maxWidth: 1045,
-        minWidth: 290,
+        minWidth: 1045,//290,
+        minHeight: 1000,
         backgroundColor: 'lightblue',
       }}
     >
@@ -44,37 +63,37 @@ export default function ImgMediaCard({ category, rev, toggleItem }) {
         height="140"
         image="/static/images/cards/contemplative-reptile.jpg"
       /> */}
-      <CardActions>
+      {/* <CardActions>
         <Button size="medium">
           <span className="material-symbols-outlined">share</span>
         </Button>
-        {rev.url &&( rev.url.includes('http') || rev.url.includes('www')?
-         (
-          <Button
-            href={rev.url}
-            target="_blank"
-            size="medium"
-            variant="outlined"
-          >
-            <span className="material-symbols-outlined">Forward</span>
-          </Button>
-        ) : (
-          <Button
-            href={`https:\\www.${rev.url}`}
-            target="_blank"
-            size="medium"
-            variant="outlined"
-          >
-            <span className="material-symbols-outlined">Forward</span>
-          </Button>
-        ))}
+        {rev.url &&
+          (rev.url.includes('http') || rev.url.includes('www') ? (
+            <Button
+              href={rev.url}
+              target="_blank"
+              size="medium"
+              variant="outlined"
+            >
+              <span className="material-symbols-outlined">Forward</span>
+            </Button>
+          ) : (
+            <Button
+              href={`https:\\www.${rev.url}`}
+              target="_blank"
+              size="medium"
+              variant="outlined"
+            >
+              <span className="material-symbols-outlined">Forward</span>
+            </Button>
+          ))}
         <Button size="medium" onClick={() => toggleItem(rev.id)}>
           <span className="material-symbols-outlined">cancel</span>
         </Button>
-      </CardActions>
+      </CardActions> */}
 
       <CardContent>
-        <Box
+        {/* <Box
           sx={{
             display: 'flex',
             alignItems: 'center',
@@ -83,8 +102,8 @@ export default function ImgMediaCard({ category, rev, toggleItem }) {
           }}
         >
           <Typography gutterBottom variant="p" component="div">
-            <div style={{ display: 'flex', 'flexFlow': 'row nowrap' }}>
-              {kategori}:-:
+            <div style={{ display: 'flex', flexFlow: 'row nowrap' }}>
+              {kategori}
               {caption}
             </div>
           </Typography>
@@ -103,31 +122,40 @@ export default function ImgMediaCard({ category, rev, toggleItem }) {
               value={null}
               readOnly
               fontSize="inherit"
+              size="large"
             />
           )}
-        </Box>
-       
-      </CardContent>
-      <CardContent>
-      <Box className="proConBox">
+        </Box> */}
+      {/* </CardContent>
+      <CardContent> */}
+        <Box /* className="proConBox" */>
           <div>LIKES</div>
-          {rev.pros && rev.pros.length > 0 && (rev.pros.map((pro) => {
-            category.pros.find((catPro) => {catPro.id === pro.id && 
-            <div key={pro.id}>{catPro.value}</div>})}))}
-            </Box>
-            <Box className='proConBox'>
-        <div>DISLIKES</div>
-          {rev.cons && rev.cons.length > 0 && (rev.cons.map((con) => {
-            category.cons.find((catCon) => {catCon.id === con.id && 
-            <div key={con.id}>{catCon.value}</div>})}))}
-     
-      </Box>
+            <List>
+          {pros.map((pro) => {
+              <ListItem key={pro.id}>
+                <ListItemText primary={pro.value} />
+              </ListItem>
+            })}
+            </List>
+        </Box>
+        {/* <Box className="proConBox">
+          <div>DISLIKES</div>
+          <List>
+          {cons &&
+            cons.length > 0 &&
+            cons.map((con) => {
+              <ListItem key={con.id}>
+                <ListItemText primary={con.value} />
+              </ListItem>;
+            })}
+            </List>
+        </Box> */}
       </CardContent>
-      <CardContent>
+      {/* <CardContent>
         <Typography variant="body2" color="text.secondary">
           {Text}
         </Typography>
-      </CardContent>
+      </CardContent> */}
     </Card>
   );
 }
